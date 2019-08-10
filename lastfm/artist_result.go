d3a6eeb47807fd8cd51c9d9f835c9dd782a75423
Kaleb Klein <klein.jae@gmail.com>
2019-08-10 01:42:27 -0400

    test
    Signed-off-by: Kaleb Klein <klein.jae@gmail.com>

--- lastfm/artist_result.go
+++ lastfm/artist_result.go
@@ -34,7 +34,7 @@ type ArtistGetInfo struct {
 	Stats      struct {
 		Listeners string `xml:"listeners"`
 		Plays     string `xml:"playcount"`
-		UserPlays string `xml:"userplaycount"`
+		UserPlays string `xml:"userplayccccount"` // test
 	} `xml:"stats"`
 	//Similar struct {
 	//Artists []struct {
